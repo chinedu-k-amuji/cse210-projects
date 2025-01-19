f6e9cbdc66eb301b4e58d91158ed1add5439fd8b
chinedu-k-amuji <amujichinedu99@gmail.com>
2025-01-19 09:09:48 +0100

    Week 2 assignment on Resume

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    // Member variables
+    public string _name;
+    public List<Job> _jobs;
+
+    // Constructor to initialize the list of jobs
+    public Resume()
+    {
+        _jobs = new List<Job>();
+    }
+
+    // Display method
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
